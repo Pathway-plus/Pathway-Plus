40a859fc1604884b25f923ff540b6068e4e0f887
YadanarLin <106858849+gadatt@users.noreply.github.com>
2022-10-11 22:05:22 +0900

    base setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,12 +9,29 @@ const Home: NextPage = () => {
 				<title>Pathway Plus</title>
 				<meta name="description" content="Pathway Plus" />
 				<link rel="icon" href="/favicon.ico" />
-			</Head>
-
+			</Head>	
+			<div className={styles.container}>
+			<header className={styles.header}>
+				<a>
+          Header
+				</a>
+			</header>
+		</div>
+			
 			<main className={styles.main}>
 				<h1 className={styles.title}>
           Welcome to Pathway Plus!
 				</h1>
+				<div>
+				<p className={styles.description}>Planning to study abroad? This is the right place for your goal!</p>
+				</div>
+				<div className={styles.grid}>
+					<a href="#" className={styles.card}>
+						<h2>Explore Now</h2>
+					</a>
+				</div>
+
+				
 
 				<p className={styles.description}>
           Links: {" "}
@@ -22,14 +39,18 @@ const Home: NextPage = () => {
 					<a href="/consulting/booking" className={styles.link}>bookings, </a>
 				</p>
 
+				<p className={styles.description}>
+				Our Services
+			</p>
+
 				<div className={styles.grid}>
-					<a href="https://nextjs.org/docs" className={styles.card}>
-						<h2>Documentation &rarr;</h2>
+					<a href="/consulting" className={styles.card}>
+						<h2>Consultation &rarr;</h2>
 						<p>Find in-depth information about Next.js features and API.</p>
 					</a>
 
 					<a href="https://nextjs.org/learn" className={styles.card}>
-						<h2>Learn &rarr;</h2>
+						<h2>Proofreading &rarr;</h2>
 						<p>Learn about Next.js in an interactive course with quizzes!</p>
 					</a>
 
@@ -37,21 +58,15 @@ const Home: NextPage = () => {
 						href="https://github.com/vercel/next.js/tree/canary/examples"
 						className={styles.card}
 					>
-						<h2>Examples &rarr;</h2>
+						<h2>Events &rarr;</h2>
 						<p>Discover and deploy boilerplate example Next.js projects.</p>
-					</a>
-
-					<a
-						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-						className={styles.card}
-					>
-						<h2>Deploy &rarr;</h2>
-						<p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-						</p>
 					</a>
 				</div>
 			</main>
+
+			<p className={styles.description}>
+				Our Consultants
+			</p>
 
 			<footer className={styles.footer}>
 				<a>
