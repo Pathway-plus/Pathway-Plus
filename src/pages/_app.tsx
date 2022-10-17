efce47f86713ba1700ab635c3c281e74f827c817
Phone Myat Kyaw <phonemyatkyawnew@gmail.com>
2022-10-17 12:25:16 +0630

    Added Context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+
 import Layout from "../components/layout";
+import AppContext from "../utils/AppContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const initialValue = {};
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AppContext.Provider value={initialValue}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AppContext.Provider>
   );
 }
 
