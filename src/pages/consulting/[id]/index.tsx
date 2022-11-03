d57c099fe992f87835ceed27d09b141a9fdd8758
Phone Myat Kyaw <phonemyatkyawnew@gmail.com>
2022-11-03 15:31:50 +0630

    Improved styles

--- src/pages/consulting/[id]/index.tsx
+++ src/pages/consulting/[id]/index.tsx
@@ -26,29 +26,29 @@ const ConsultantDetail: NextPage<ServerSideProps> = ({ consultant, id }: ServerS
       <div className="group relative flex justify-center bg-black bg-opacity-50">
         <Image src="/assets/consulting.jpg" layout="fill" alt="Consultation background" className="group-hover:scale-105 object-cover transition-transform -z-10" />
         <div className="p-16">
-          <h1 className="text-4xl font-semibold text-white">Consultant Information Detail</h1>
+          <h1 className="text-center md:text-4xl text-3xl font-semibold text-white">Consultant Information Detail</h1>
         </div>
       </div>
 
-      <div className="sm:flex-row flex flex-col p-12">
-        <div className="sm:mr-12 flex-1 mr-0">
-          <div className="sm:h-[25vw] relative w-full h-[60vw] mb-12">
+      <div className="md:flex-row flex flex-col gap-12 p-12">
+        <div className="w-full mr-0">
+          <div className="relative w-full h-[50vw]">
             {/* <Image src={consultant.src} layout="fill" className="object-top object-contain"/> */}
             <Image src={"/assets/man.png"} layout="fill" className="object-top object-contain"/>
           </div>
         </div>
-        <div className="flex-[3]">
-          <h2 className="font-semibold text-xl mb-6">{consultant.name}</h2>
-          <ol className="w-96 mb-6 space-y-2">
+        <div className="space-y-6">
+          <h2 className="font-semibold text-xl">{consultant.name}</h2>
+          <ol className="md:w-96 space-y-4">
             <DetailItem label="University" value={consultant.university} />
             <DetailItem label="Specialization" value={consultant.major} />
             <DetailItem label="Year" value={consultant.year} />
             <DetailItem label="Country" value={consultant.country} />
           </ol>
-          <h3 className="font-semibold text-lg mb-6">Introduction</h3>
-          <p className="mb-12">{consultant.introduction}</p>
+          <h3 className="font-semibold text-lg">Introduction</h3>
+          <p className="text-justify">{consultant.introduction}</p>
           <Link href={{ pathname: `/consulting/booking/${id}` }}>
-            <button className="w-80 py-3 rounded-lg text-white bg-primary-light hover:bg-primary shadow-default hover:shadow-shrink transition-all">
+            <button className="md:w-80 w-full py-3 rounded-lg text-white bg-primary-light hover:bg-primary shadow-default hover:shadow-shrink transition-all">
               Make Appointment
             </button>
           </Link>
@@ -60,9 +60,9 @@ const ConsultantDetail: NextPage<ServerSideProps> = ({ consultant, id }: ServerS
 
 const DetailItem = ({ label, value }: { label: string, value: string }) => {
   return (
-    <li className="flex">
-      <span className="flex flex-1">{label}</span>
-      <div className="mr-10 w-2 h-2 mt-2 rounded-xl bg-primary" />
+    <li className="flex md:flex-row flex-col">
+      <span className="flex md:flex-1 font-semibold md:font-normal">{label}</span>
+      <div className="md:flex hidden mr-10 w-2 h-2 mt-2 rounded-xl bg-primary" />
       <span className="flex flex-[1.5]">{value}</span>
     </li>
   );
--- src/pages/consulting/index.tsx
+++ src/pages/consulting/index.tsx
@@ -45,20 +45,20 @@ const Consulting: NextPage<ServerSideProps> = () => {
     <div>
       <div className="group relative flex mb-4 justify-center">
         <Image src="/assets/consulting.jpg" layout="fill" alt="Consultation background" className="group-hover:scale-105 object-cover transition-transform" />
-        <div className="relative flex flex-col p-10 space-y-12 max-w-2xl items-center">
-          <h1 className="text-4xl font-semibold text-white">What is Consultation?</h1>
-          <p className="text-white text-lg">
+        <div className="relative flex flex-col p-10 gap-y-12 md:max-w-2xl w-11/12 items-center">
+          <h1 className="md:text-4xl text-3xl font-semibold text-white text-center">What is Consultation?</h1>
+          <p className="text-white md:text-lg text-sm">
 						Lacus mattis odio sem fusce convallis vitae aliquam tempor. Quam ut odio
 						eget eu amet. Aenean tortor amet sit lorem. Nibh pharetra lorem tellus nisl 
 						feugiat eu.
           </p>
-          <div className="flex w-full">
+          <div className="flex w-full flex-wrap gap-2">
             <form className="flex flex-1" onSubmit={filterName}>
               <input type="search" name="search" id="search" placeholder="Search your consultants" className="flex-1 px-3 py-1.5 outline-none rounded-l-md bg-slate-200" />
               {/* Replace emoji with vector icon */}
-              <button type="submit" className="w-10 mr-2 bg-primary-light hover:bg-primary transition-colors rounded-r-md">🔎</button>
+              <button type="submit" className="w-10 bg-primary-light hover:bg-primary transition-colors rounded-r-md">🔎</button>
             </form>
-            <select onChange={filterCountry} id="select_country" className="px-1 bg-slate-200 text-primary font-semibold">
+            <select onChange={filterCountry} id="select_country" className="p-1 bg-slate-200 text-primary font-semibold">
               <option disabled value="">Choose country</option>
               {tempPlaceholderCountries.map(country => <option key={country} value={country}>{country}</option>)}
             </select>
@@ -67,7 +67,7 @@ const Consulting: NextPage<ServerSideProps> = () => {
       </div>
 
       <div>
-        <h1 className="text-center text-3xl font-semibold">Meet Our Consultants</h1>
+        <h1 className="text-center md:text-3xl text-2xl font-semibold">Meet Our Consultants</h1>
         <div className="flex flex-wrap justify-center">
           {noConsultantsFound 
             ? <p className="py-36">No consultants found...</p> 
@@ -84,7 +84,7 @@ const Consulting: NextPage<ServerSideProps> = () => {
 const Card = ({ data } : { data: Consultant }) => {
   return data._id !== undefined ? (
     <Link href={{ pathname: `/consulting/${data._id}` }}>
-      <div className="group active:scale-105 flex flex-col w-72 px-10 py-4 m-6 mt-20 space-y-4 items-center rounded-lg cursor-pointer shadow-default hover:shadow-expand active:shadow-shrink transition-all">
+      <div className="group flex flex-col w-72 px-10 py-4 m-6 mt-20 gap-y-4 items-center rounded-lg cursor-pointer shadow-default hover:shadow-expand active:shadow-shrink transition-all">
         <div className="group-hover:scale-105 transition-transform relative -mt-20 w-36 h-36">
           {/* <Image src={data.profile} layout="fill" alt={`Picture of ${data.name}`}  className="object-cover rounded-full" /> */}
           <Image src={"/assets/man.png"} layout="fill" alt={`Picture of ${data.name}`}  className="object-cover rounded-full" />
