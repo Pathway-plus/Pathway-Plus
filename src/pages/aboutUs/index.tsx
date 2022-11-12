7ff51eda0b56a785887b13fdfd8f0670231b66eb
YadanarLin <106858849+gadatt@users.noreply.github.com>
2022-11-12 23:17:20 +0900

    added bulleted list

--- src/pages/aboutUs/index.tsx
+++ src/pages/aboutUs/index.tsx
@@ -25,9 +25,64 @@ const AboutUs:NextPage =() =>{
       <div>
         <p className="px-5 py-5 text-center font-semibold text-sm">Our Missions</p>
       </div>
-      <span className="-z-10 relative left-0 px-10"><Image src={"/assets/about-us/demo.png"} width="200" height="150"></Image></span>
+      <div className="flex">
+        <span className="relative left-12 w-1/2"><Image src={"/assets/about-us/demo.png"} width="350" height="250"></Image></span>
 
-      <div>
+        <div className="w-1/2 float-right mr-16 font-light">
+
+          <div className="flex float-right py-2 mr-6">
+            <span className="relative float-left"><Image src={"/assets/about-us/Vector.png"} width="20" height="20"></Image></span>
+            <span className="mx-2 float-right text-xs">
+			To empower Burmese youths by providing virtual
+              <br />
+			academic consultations and career events regardless
+              <br />
+			of race,religion,and ethnicity.
+            </span>
+
+          </div>
+
+          <div className="flex float-right py-2">
+            <span className="relative float-left"><Image src={"/assets/about-us/Vector.png"} width="20" height="20"></Image></span>
+            <span className="mx-2 float-right text-xs">
+	To establish the community not only to provide Burmese
+              <br />
+			youths through on-ground vocational training
+              <br />
+			programmes,and academic and career-related-
+              <br />
+			programmes,but also to assists mid-career changes for
+              <br />
+			experienced employees in terms of training and academic
+              <br />
+			programmes.
+            </span>
+          </div>
+
+          <div className="flex float-right py-2 mr-[5px]">
+            <span className="relative float-left"><Image src={"/assets/about-us/Vector.png"} width="20" height="20"></Image></span>
+            <span className="mx-2 float-right text-xs">
+ To undertake any research necessary to futher any of the
+              <br />
+ objects specified above
+            </span>
+          </div>
+
+          <div className="flex float-right py-2 mr-6">
+            <span className="relative float-left"><Image src={"/assets/about-us/Vector.png"} width="20" height="20"></Image></span>
+            <span className="mx-2 float-right text-xs">
+ To do all such other lawful things as are incidental or
+              <br />
+conducive to the attainment of any or all of the above
+              <br />
+ objects.
+            </span>
+          </div>
+
+        </div>
+      </div>
+
+      <div className ="px-5 py-5 text-center font-semibold text-sm">
         <p className="px-5 py-5 text-center font-semibold text-sm">Head Of Organizations</p>
         <br />
       </div>
